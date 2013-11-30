bccbc168568afe072a2025d251f51394a385209d
Ricky Reusser <rsreusser@gmail.com>
2013-11-30 13:44:09 -0500

    S3 deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,6 +58,33 @@ module.exports = function (grunt) {
       }
     },
 
+    aws: grunt.file.readJSON('./aws.json'),
+    s3: {
+        options: {
+            key: '<%= aws.key %>',
+            secret: '<%= aws.secret %>',
+            bucket: '<%= aws.bucket %>',
+            access: 'public-read',
+            headers: {
+                // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+                'Cache-Control': 'max-age=630720000, public',
+                'Expires': new Date(Date.now() + 63072000000).toUTCString()
+            }
+        },
+        dist: {
+            options: {
+                encodePaths: false,
+                maxOperations: 20
+            },
+            upload: [{
+                src: 'dist/**',
+                dest: '',
+                rel: 'dist',
+                options: { gzip: true }
+            }]
+        }
+    },
+
     // The actual grunt server settings
     connect: {
       options: {
@@ -412,7 +439,8 @@ module.exports = function (grunt) {
     'cssmin',
     'uglify',
     'rev',
-    'usemin'
+    'usemin',
+    's3'
   ]);
 
   grunt.registerTask('default', [
